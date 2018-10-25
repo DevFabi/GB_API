915a0c3d51da3b821a1ad360af1e8018bb3b1bcb
Fabiola BELET <fabiola.belet@gmail.com>
2018-10-25 15:04:01 -0400

    Part 2 :: Authentification

--- api/config/core.php
+++ api/config/core.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
  
 // home page url
-$home_url="http://localhost/TWO-RESTAPIPHP/api/";
+$home_url="http://localhost/GB_API/api/";
  
 // page given in URL parameter, default page is one
 $page = isset($_GET['page']) ? $_GET['page'] : 1;
@@ -14,4 +14,15 @@ $records_per_page = 5;
  
 // calculate for the query LIMIT clause
 $from_record_num = ($records_per_page * $page) - $records_per_page;
+
+// set your default time-zone
+date_default_timezone_set('Asia/Manila');
+ 
+// variables used for jwt
+$key = "example_key";
+$iss = "http://example.org";
+$aud = "http://example.com";
+$iat = 1356999524;
+$nbf = 1357000000;
+
 ?>
